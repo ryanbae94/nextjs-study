22d2ee1ab225159a517e4850959913fa93ffaab7
ryanbae94 <ryanbae94@gmail.com>
2024-01-04 19:19:42 +0900

    mock setting

---
+++ src/app/_component/MSWComponent.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React, { useEffect } from 'react';
+
+export default function MSWComponent() {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+        require('@/mocks/browser');
+      }
+    }
+  }, []);
+  return null;
+}
--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,11 +1,26 @@
+import { faker } from '@faker-js/faker';
 import { HttpResponse, http } from 'msw';
+
+const User = [
+  { id: 1, nickname: 'Elon Musk', image: faker.image.avatar() },
+  { id: 2, nickname: 'Ryan Bae', image: faker.image.avatar() },
+];
 
 export const handlers = [
   http.post('/api/login', () => {
     console.log('로그인');
     return HttpResponse.json(User[1], {
       headers: {
-        'Set-Cookie': 'connect.sid=msw',
+        'Set-Cookie': 'connect.sid=msw;HttpOnly;Path=/',
+      },
+    });
+  }),
+
+  http.post('/api/logout', () => {
+    console.log('로그아웃');
+    return new HttpResponse.json(null, {
+      headers: {
+        'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0',
       },
     });
   }),
